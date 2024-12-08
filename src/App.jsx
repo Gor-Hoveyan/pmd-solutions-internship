1aa5edf8e9833cc72136e0ce3b6e8318785f5630
Gor-Hoveyan <gorpro2004@mail.ru>
2024-12-08 23:20:48 +0400

    Implement stats and charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import styles from "./App.module.scss";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Main from "./pages/main/Main";
 
 function App() {
   return (
     <main className={styles.main}>
       <div className={styles.layout}>
         <Navbar />
-        <Header />
+        <div className={styles.container}>
+          {" "}
+          <Header />
+          <Main />
+        </div>
       </div>
     </main>
   );
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,18 @@
+import ExpenseStats from "../../components/expenseStats/ExpenseStats";
+import Savings from "../../components/savings/Savings";
+import WeeklyActivity from "../../components/weeklyActivity/WeeklyActivity";
+import MostSelling from "./../../components/mostSelling/MostSelling";
+import RecentOrders from "./../../components/recentOrders/RecentOrders";
 import styles from "./Main.module.scss";
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main className={styles.main}>
+      <MostSelling />
+      <RecentOrders />
+      <WeeklyActivity />
+      <ExpenseStats />
+      <Savings />
+    </main>
+  );
 }
