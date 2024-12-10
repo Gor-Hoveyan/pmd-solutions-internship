a6c3059d81c664c7c0c98449381036f827384bd5
Gor-Hoveyan <gorpro2004@mail.ru>
2024-12-10 17:37:52 +0400

    Implement routing system

--- src/components/navbar/lists/desktopList/DesktopList.jsx
+++ src/components/navbar/lists/desktopList/DesktopList.jsx
@@ -6,43 +6,93 @@ import reviews from "./../../../../assets/navbar/desktop/reviews.svg";
 import statistics from "./../../../../assets/navbar/statistics.svg";
 import currencies from "./../../../../assets/navbar/currencies.svg";
 import setting from "./../../../../assets/navbar/setting.svg";
+import { NavLink } from "react-router";
 
-export default function DesktopList() {
+export default function DesktopList({ path }) {
   return (
     <ul className={styles.list}>
-      <li className={styles.active}>
-        <img className={styles.img} src={dashboard} alt="Dashboard vector" />
-        Dashboard
+      <li>
+        <NavLink
+          to={"/"}
+          className={`${path === "/" ? styles.active : ""} ${styles.navLink}`}
+        >
+          <img className={styles.img} src={dashboard} alt="Dashboard vector" />
+          Dashboard
+        </NavLink>
       </li>
       <li>
-        {" "}
-        <img className={styles.img} src={orders} alt="Orders vector" />
-        Orders
+        <NavLink
+          to={"/orders"}
+          className={`${path === "/orders" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img className={styles.img} src={orders} alt="Orders vector" />
+          Orders
+        </NavLink>
       </li>
       <li>
-        {" "}
-        <img className={styles.img} src={products} alt="Products vector" />
-        Products
+        <NavLink
+          to={"/products"}
+          className={`${path === "/products" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img className={styles.img} src={products} alt="Products vector" />
+          Products
+        </NavLink>
       </li>
       <li>
-        {" "}
-        <img className={styles.img} src={reviews} alt="Reviews vector" />
-        Reviews
+        <NavLink
+          to={"/reviews"}
+          className={`${path === "/reviews" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img className={styles.img} src={reviews} alt="Reviews vector" />
+          Reviews
+        </NavLink>
       </li>
       <li>
-        {" "}
-        <img className={styles.img} src={statistics} alt="Statistics vector" />
-        Statistics
+        <NavLink
+          to={"/statistics"}
+          className={`${path === "/statistics" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img
+            className={styles.img}
+            src={statistics}
+            alt="Statistics vector"
+          />
+          Statistics
+        </NavLink>
       </li>
-      <li>
-        {" "}
-        <img className={styles.img} src={currencies} alt="Currencies vector" />
-        Currencies
+      <li className={path === "/currencies" ? styles.active : ""}>
+        <NavLink
+          to={"/currencies"}
+          className={`${path === "/currencies" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img
+            className={styles.img}
+            src={currencies}
+            alt="Currencies vector"
+          />
+          Currencies
+        </NavLink>
       </li>
-      <li>
-        {" "}
-        <img className={styles.img} src={setting} alt="Setting vector" />
-        Setting
+      <li className={path === "/setting" ? styles.active : ""}>
+        <NavLink
+          to={"/setting"}
+          className={`${path === "/setting" ? styles.active : ""} ${
+            styles.navLink
+          }`}
+        >
+          <img className={styles.img} src={setting} alt="Setting vector" />
+          Setting
+        </NavLink>
       </li>
     </ul>
   );
