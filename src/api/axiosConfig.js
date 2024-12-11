bb7c2336e07be6ae9b6f953f546e6f14e8f51cfe
Gor-Hoveyan <gorpro2004@mail.ru>
2024-12-11 23:13:53 +0400

    Add loging system

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://zangak.pmdsolutions.org",
+  withCredentials: true,
+});
+
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
+
+export default api;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Navigate } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      {!(
+        localStorage.getItem("token") && localStorage.getItem("token").length
+      ) ? (
+        <Navigate to="/login" />
+      ) : (
+        ""
+      )}
     </BrowserRouter>
   </StrictMode>
 );
